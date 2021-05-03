66530d970a1ea149d50b4d3709e7b0c8654b1fac
blacksempai <benjaminfranclin1998@gmail.com>
2021-05-03 16:51:50 +0300

    Test simple json return

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+import express from "express";
+import fs from 'fs';
+import path from 'path';
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server has been started on ${PORT}...`);
+})
+
+app.get('/api/notes', (req: express.Request, res: express.Response) => {
+    fs.readFile(path.join(__dirname,'models','notes.json'),(err,data)=>{
+        if(err){
+            console.error(err);
+            res.writeHead(500);
+            res.end();
+        }
+        else {
+            res.end(data);
+        }
+    })
+})
