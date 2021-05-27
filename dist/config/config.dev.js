bb7dc1d0fef3824e59234d3fee271816eb3257c7
blacksempai <benjaminfranclin1998@gmail.com>
2021-05-27 23:08:48 +0300

    prod commit

--- dist/config/config.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    mongoURI: 'mongodb+srv://root:root@cluster0.scuw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    jwt: 'dev-jwt'
-};
