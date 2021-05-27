97e0b8e7a190f409f7ad15d6a9f87b48004dc016
blacksempai <benjaminfranclin1998@gmail.com>
2021-05-27 23:15:53 +0300

    prod commit

--- src/app.ts
+++ src/app.ts
@@ -23,11 +23,10 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/note', noteRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist/client'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'client', 'index.html'));
-    });
-}
+app.use(express.static('client/dist/client'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'client', 'index.html'));
+});
+
 
 export { app };
