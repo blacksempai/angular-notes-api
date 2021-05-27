14585b02569ddd13c95c7781b3158d3a77b1adc0
blacksempai <benjaminfranclin1998@gmail.com>
2021-05-28 00:55:17 +0300

    prod commit

--- dist/utils/error-handler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (res, err) {
-    res.status(500).json({
-        success: false,
-        error: err.message ? err.message : err
-    });
-});
